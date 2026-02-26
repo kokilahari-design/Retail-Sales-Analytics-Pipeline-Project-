fadaa7a83ba8b4e5d9530813cb3b1357319c2f61
kokilahari-design <kokila2065@gmail.com>
2026-02-27 02:40:18 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.routes.sales_routes import sales_bp
-
-def create_app():
-    app = Flask(__name__)
-    
-    app.register_blueprint(sales_bp, url_prefix="/api")
-
-    return app
